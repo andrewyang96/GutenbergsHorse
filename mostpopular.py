04c12724668c96267c731359fd14fbace9146f64
Andrew Yang <ayang14@illinois.edu>
2015-06-20 15:07:45 -0500

    Update book list

--- mostpopular.py
+++ mostpopular.py
@@ -99,4 +99,103 @@ MOSTPOPULAR = [
     1998,   # Thus Spake Zarathustra
     2148,   # The Works of Edgar Allan Poe - Volume 2
     245,    # Life on the Mississippi
+    3300,   # An Inquiry into the Nature and Causes of the Wealth of Nations
+    62,     # A Princess of Mars
+    108,    # The Return on Sherlock Holmes
+    2781,   # Just So Stories
+    996,    # Don Quixote
+    19942,  # Candide
+    215,    # The Call of the Wild
+    408,    # The Souls of Black Folk
+    3207,   # Leviathan
+    10007,  # Carmilla
+    141,    # Mansfield Park
+    35123,  # The Ladies' Book of Etiquette, a Manual of Politeness
+    3296,   # The Confessions of St. Augustine
+    2413,   # Madame Bovary
+    514,    # Little Women
+    1257,   # The Three Musketeers
+    2097,   # The Sign of the Four
+    805,    # This Side of Paradise
+    45631,  # Twelve Years a Slave
+    851,    # Narrative of the Captivity and Restoration of Mrs. Mary Rowlandson
+    145,    # Middlemarch
+    121,    # Northanger Abbey
+    8799,   # The Divine Comedy by Dante
+    1951,   # The Coming Race
+    375,    # An Occurrence at Owl Creek Bridge
+    40311,  # China and Pottery Marks
+    #1112,  # The Tragedy of Romeo and Juliet
+    14591,  # Faust
+    #18251, # Latin for Beginners
+    2680,   # Meditations
+    #2000,  # Don Quijote
+    86,     # A Connecticut Yankee in King Arthur's Court
+    34901,  # On Liberty
+    33,     # The Scarlet Letter
+    3176,   # The Innocents Abroad
+    140,    # The Jungle
+    37134,  # The Elements of Style
+    8492,   # The King in Yellow
+    15399,  # The Interesting Narrative of the Life of Olaudah Equiano, or Gustavus Vassa, the African
+    786,    # Hard Times
+    147,    # Common Sense
+    1727,   # The Odyssey
+    15250,  # Myths and Legends of China
+    110,    # Tess of the d'Urbervilles: A Pure Woman
+    14838,  # The Tale of Peter Rabbit
+    5827,   # The Problems of Philosophy
+    526,    # Heart of Darkness
+    20480,  # Palmistry for All
+    7178,   # Swann's Way
+    #28233, # Philosophiae Naturalis Principia Mathematica
+    4705,   # A Treatise of Human Nature
+    1228,   # On the Origin of Species
+    22577,  # Practical Grammar and Composition
+    7849,   # The Trial
+    3177,   # Roughing It
+    209,    # The Turn of the Screw
+    834,    # The Memoirs of Sherlock Holmes
+    1597,   # Andersen's Fairy Tales
+    4280,   # The Critique of Pure Reason
+    31547,  # Youth
+    4517,   # Ethan Frome
+    30142,  # Little Brother
+    21279,  # Kurt Vonnegut
+    1251,   # Le Morte d'Arthur: Volume 1
+    5000,   # The Notebooks of Leonardo Da Vinci
+    19033,  # Alice's Adventures in Wonderland
+    113,    # The Secret Garden
+    972,    # The Devil's Dictionary
+    885,    # An Ideal Husband
+    289,    # The Wind in the Willows
+    779,    # The Tragical History of Doctor Faustus
+    421,    # Kidnapped
+    1023,   # Bleak House
+    139,    # The Lost World
+    7370,   # Second Treatise of Government
+    164,    # Twenty Thousand Leagues Under the Sea
+    #1524,  # Hamlet Prince of Denmark
+    #2264,  # Macbeth
+    2848,   # Antiquities of the Jews
+    203,    # Uncle Tom's Cabin
+    107,    # Far from the Madding Crowd
+    19068,  # Household Stories by the Brothers Grimm
+    14264,  # The Practice and Science of Drawing
+    155,    # The Moonstone
+    12299,  # The Mechanical Properties of Wood
+    2638,   # The Idiot
+    20583,  # The Tribes and Castes of the Central Provinces of India
+    2610,   # Notre-Dame de Paris
+    41617,  # A Complete Guide to Heraldry
+    34206,  # The Thousand and One Nights, Vol. I
+    1250,   # Anthem
+    600,    # Notes from the Underground
+    49104,  # The History of the Crusades, Vol. 1 of 3
+    2130,   # Utopia
+    2428,   # An Essay on Man; Moral Essays and Satires
+    3800,   # Ethica
+    #10625, # A Concise Dictionary of Middle English from A.D. 1150 to 1580
+    541,    # The Age of Innocence
+    49057,  # Tales of King Arthur and the Round Table
 ]
