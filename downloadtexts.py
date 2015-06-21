2fb4dedf479ab60dc3188f42f48a713c01e9887d
Andrew Yang <ayang14@illinois.edu>
2015-06-21 00:25:51 -0500

    Cleanup sentence implemented

--- downloadtexts.py
+++ downloadtexts.py
@@ -23,24 +23,36 @@ def filterSentenceFunc(sentence):
     # 2. Sentence has at most 140 characters.
     return countWords(sentence) >= 2 and len(sentence) <= 140
 
+def findFirstLettersIdx(sentence):
+    letters = re.findall(r'[a-zA-Z]', sentence)
+    return sentence.find(letters[0])
+
+def ensureFirstLetterUpper(sentence):
+    flIdx = findFirstLettersIdx(sentence)
+    s = list(sentence)
+    s[flIdx] = s[flIdx].upper()
+    return ''.join(s)
+
 def cleanupSentence(sentence):
     # Clean up sentence by doing the following:
     # 1. Resolve dangling quotation marks at either the front or end of the sentence.
+    if sentence[0] == '"' and '"' not in sentence[1:]:
+        sentence += '"'
+    elif sentence[-1] == '"' and '"' not in sentence[:-1]:
+        sentence = '"' + sentence
     # 2. Resolve dangling parenthesis at either the front or end of the sentence.
+    if "(" in sentence and ")" not in sentence:
+        sentence += ")"
+    elif ")" in sentence and "(" not in sentence:
+        sentence = '(' + sentence
     # 3. Resolve dangling brackets at either the front or end of the sentence.
+    if "[" in sentence and "]" not in sentence:
+        sentence += "]"
+    elif "]" in sentence and "[" not in sentence:
+        sentence = "[" + sentence
     # 4. Capitalize the first letter of the sentence (may not be at index 0).
-    return sentence # TODO: change
-    # Test cases:
-    # "She is well, but sad.
-    # "I'm not a bit changed--not really.
-    # GREEK CUT OPEN-WORK PATTERN (fig.
-    # WHEEL BEGUN IN HOLE GROUND.]
-    # while he storms, 't is well \nThat thou descend."
-    # my nice \nwaistcoat!"
-    # Shouldn't change:
-    # But have I now seen Death?
-    # "I am called," said Andrea.
-    # "Well," said Andrea, "admitting your love, why do you want me to breakfast with you?"
+    sentence = ensureFirstLetterUpper(sentence)
+    return sentence
 
 def tokenizeText(splitter, text):
     initList = [unicode(sentence).strip() for sentence in splitter.tokenize(text)]
@@ -60,8 +72,32 @@ def retrieveSentences():
     texts = downloadTexts(MOSTPOPULAR)
     return tokenizeTexts(texts)
 
-if __name__ == "__main__":
+def downloadTexts():
     sents = retrieveSentences()
     with open(os.path.join(os.getcwd(), "gutenbergpickle.bin"), 'w') as f:
         print "Dumping to file"
         cPickle.dump(sents, f)
+
+def runTestcases():
+    cases = [
+        '"She is well, but sad.',
+        '"I\'m not a bit changed--not really.',
+        "GREEK CUT OPEN-WORK PATTERN (fig.",
+        "WHEEL BEGUN IN HOLE GROUND.]",
+        'while he storms, \'t is well \nThat thou descend."',
+        'my nice \nwaistcoat!"',
+        "Shouldn't change:",
+        "But have I now seen Death?",
+        '"I am called," said Andrea.',
+        '"Well," said Andrea, "admitting your love, why do you want me to breakfast with you?"'
+    ]
+    for case in cases:
+        print "Original"
+        print case
+        print "cleanupSentence"
+        print cleanupSentence(case)
+        print
+
+if __name__ == "__main__":
+    print "Running test cases for cleanupSentence. downloadTexts() runs the download utility."
+    runTestcases()
